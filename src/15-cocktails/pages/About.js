2e6880aa72b3a8267abedaae963633a3448804d1
ZawHtet-246098 <zhtet1636@gmail.com>
2022-04-04 14:11:01 +0630

    first commit

---
+++ src/15-cocktails/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        asperiores architecto nesciunt est eos autem quas a commodi earum cum
+        quam, tenetur aliquam nihil hic incidunt maxime odit non temporibus
+        dicta numquam. Cum voluptates earum quidem nulla unde odit
+        reprehenderit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
